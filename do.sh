0b9cfa64f8eb6b6975bf463b6a4a78d2d77ac1d8
Davide 3500 <me@davidecostantini.it>
2015-09-10 22:29:23 +0100

    Added role Jenkins and classes on Manifest

--- do.sh
+++ do.sh
@@ -42,10 +42,28 @@ rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.r
 
 if [ "$role" = "master" ]; then
 	yum install -y puppet-server
-	#yum install -y http://yum.theforeman.org/releases/1.9/el6/x86_64/foreman-release.rpm
-
-	#yum install -y foreman-installer
-	#foreman-installer -v
+	
+	puppet module install jfryman-selinux
+	puppet module install puppetlabs-firewall
+	puppet module install puppetlabs-apache
+	puppet module install jfryman-nginx
+	puppet module install puppetlabs-haproxy
+	puppet module install aptituz-ssh
+	puppet module install puppetlabs-lvm
+	puppet module install derdanne-nfs
+	puppet module install jgreat-docker
+	puppet module install evenup-beaver
+	puppet module install puppetlabs-apt
+	puppet module install datadog-datadog_agent
+	puppet module install puppetlabs-vcsrepo
+	puppet module install elasticsearch-elasticsearch
+	puppet module install elasticsearch-logstash
+	puppet module install camptocamp-kibana
+	puppet module install camptocamp-kibana
+	puppet module install wdijkerman-zabbix
+	puppet module install camptocamp-openssl
+	puppet module install thias-openvpn
+	puppet module install rtyler-jenkins
 
 	#Adding autosign
 	echo "[master]" >> /etc/puppet/puppet.conf
--- site.pp
+++ site.pp
@@ -1,6 +1,32 @@
-file { "/tmp/dave":
-    ensure => "present",
-    owner  => "root",
-    content => "default content set by puppet master",
-    mode   => 750,
+###Classes
+class common_class {
+
+	class { 'selinux':
+	 mode => 'disabled'
+	}
+
+	resources { 'firewall':
+	  purge => true
+	}
+
+	firewall { '000 accept all requests':
+	  proto  => 'all',
+	  action => 'accept'
+	}
+}
+
+##Roles
+class role_webserver {
+	include common_class 
+}
+
+class role_jenkins {
+	include common_class 
+	include jenkins
+}
+
+
+###Start
+node 'www1.example.com' {
+	include role_jenkins
 }
